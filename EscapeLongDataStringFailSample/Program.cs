0ba5ab55a2284afad7b342a1716cafafc701e05e
mifumi323 <mifumi323@gmail.com>
2017-05-05 20:40:51 +0900

    失敗パターンのサンプル

---
+++ EscapeLongDataStringFailSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EscapeLongDataStringFailSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringToEscape = "なが".PadRight(99996, 'ー') + "い文字列";
+            var result = Uri.EscapeDataString(stringToEscape);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
